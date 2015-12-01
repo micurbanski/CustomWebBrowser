d370cd9cc43be12619dff50cd8c7cbbf6081a692
urb4nm <urb4nm@gmail.com>
2015-12-01 11:28:15 +0100

    Refactored Code

--- CustomWebBrowser/MainForm.Designer.cs
+++ CustomWebBrowser/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CustomWebBrowser
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.adressToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.adressBox = new System.Windows.Forms.ToolStripComboBox();
             this.goToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.addPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.removePageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.mainTabControl = new System.Windows.Forms.TabControl();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +44,7 @@
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.adressToolStripMenuItem,
-            this.toolStripComboBox1,
+            this.adressBox,
             this.goToolStripMenuItem,
             this.toolStripMenuItem1,
             this.toolStripMenuItem2,
@@ -62,60 +62,67 @@
             this.adressToolStripMenuItem.Size = new System.Drawing.Size(54, 23);
             this.adressToolStripMenuItem.Text = "Adress";
             // 
-            // toolStripComboBox1
+            // adressBox
             // 
-            this.toolStripComboBox1.Name = "toolStripComboBox1";
-            this.toolStripComboBox1.Size = new System.Drawing.Size(450, 23);
+            this.adressBox.Name = "adressBox";
+            this.adressBox.Size = new System.Drawing.Size(450, 23);
+            this.adressBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.adressBox_KeyDown);
             // 
             // goToolStripMenuItem
             // 
             this.goToolStripMenuItem.Name = "goToolStripMenuItem";
             this.goToolStripMenuItem.Size = new System.Drawing.Size(34, 23);
             this.goToolStripMenuItem.Text = "Go";
+            this.goToolStripMenuItem.Click += new System.EventHandler(this.goToolStripMenuItem_Click);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Size = new System.Drawing.Size(35, 23);
             this.toolStripMenuItem1.Text = "<<";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
             // toolStripMenuItem2
             // 
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
             this.toolStripMenuItem2.Size = new System.Drawing.Size(35, 23);
             this.toolStripMenuItem2.Text = ">>";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
             // 
             // addPageToolStripMenuItem
             // 
             this.addPageToolStripMenuItem.Name = "addPageToolStripMenuItem";
             this.addPageToolStripMenuItem.Size = new System.Drawing.Size(70, 23);
             this.addPageToolStripMenuItem.Text = "Add Page";
+            this.addPageToolStripMenuItem.Click += new System.EventHandler(this.addPageToolStripMenuItem_Click);
             // 
             // removePageToolStripMenuItem
             // 
             this.removePageToolStripMenuItem.Name = "removePageToolStripMenuItem";
             this.removePageToolStripMenuItem.Size = new System.Drawing.Size(91, 23);
             this.removePageToolStripMenuItem.Text = "Remove Page";
+            this.removePageToolStripMenuItem.Click += new System.EventHandler(this.removePageToolStripMenuItem_Click);
             // 
-            // tabControl1
+            // mainTabControl
             // 
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 27);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(784, 534);
-            this.tabControl1.TabIndex = 1;
+            this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTabControl.Location = new System.Drawing.Point(0, 27);
+            this.mainTabControl.Name = "mainTabControl";
+            this.mainTabControl.SelectedIndex = 0;
+            this.mainTabControl.Size = new System.Drawing.Size(784, 534);
+            this.mainTabControl.TabIndex = 1;
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.mainTabControl);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -127,13 +134,13 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem adressToolStripMenuItem;
-        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripComboBox adressBox;
         private System.Windows.Forms.ToolStripMenuItem goToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
         private System.Windows.Forms.ToolStripMenuItem addPageToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem removePageToolStripMenuItem;
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl mainTabControl;
     }
 }
 
---
+++ CustomWebBrowser/MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomWebBrowser
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        WebBrowser web = new WebBrowser();
+        int tabCount = 0;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            web = new WebBrowser();
+            web.ScriptErrorsSuppressed = true;
+            web.Dock = DockStyle.Fill;
+            web.Visible = true;
+
+            web.DocumentCompleted += web_DocumentCompleted;
+
+            mainTabControl.TabPages.Add("New Tab");
+            mainTabControl.SelectTab(tabCount);
+            mainTabControl.SelectedTab.Controls.Add(web);
+            tabCount += 1;
+        }
+
+        void web_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            mainTabControl.SelectedTab.Text = ((WebBrowser)mainTabControl.SelectedTab.Controls[0]).DocumentTitle;
+        }
+
+        private void goToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)mainTabControl.SelectedTab.Controls[0]).Navigate(adressBox.Text);
+            if (!adressBox.Items.Contains(adressBox.Text))
+            {
+                adressBox.Items.Add(adressBox.Text);
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)mainTabControl.SelectedTab.Controls[0]).GoBack();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)mainTabControl.SelectedTab.Controls[0]).GoForward();
+        }
+
+        private void addPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            web = new WebBrowser();
+            web.ScriptErrorsSuppressed = true;
+            web.Dock = DockStyle.Fill;
+            web.Visible = true;
+
+            web.DocumentCompleted += web_DocumentCompleted;
+
+            mainTabControl.TabPages.Add("New Tab");
+            mainTabControl.SelectTab(tabCount);
+            mainTabControl.SelectedTab.Controls.Add(web);
+            tabCount += 1;
+        }
+
+        private void removePageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mainTabControl.TabPages.Count - 1 > 0)
+            {
+                mainTabControl.TabPages.RemoveAt(mainTabControl.SelectedIndex);
+                mainTabControl.SelectTab(mainTabControl.TabPages.Count - 1);
+                tabCount -= 1;
+            }
+        }
+
+        private void adressBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                ((WebBrowser)mainTabControl.SelectedTab.Controls[0]).Navigate(adressBox.Text);
+                if (!adressBox.Items.Contains(adressBox.Text))
+                {
+                    adressBox.Items.Add(adressBox.Text);
+                }
+            }
+        }
+    }
+}
